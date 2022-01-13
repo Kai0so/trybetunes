db34e529d8a1c5ccb8589cf2238194a0d5d3e371
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-01-13 17:21:32 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
-
-// Start
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Router>
+        <div>
+          <p>TrybeTunes</p>
+          <li><Link to="/"> Login </Link></li>
+          <li><Link to="/search"> Search </Link></li>
+          <li><Link to="/album/:id"> Album </Link></li>
+          <li><Link to="/favorites"> Favorites </Link></li>
+          <li><Link to="/profile"> Profile </Link></li>
+          <li><Link to="/profile/edit"> Edit Profile </Link></li>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route component={ NotFound } />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
