fbe6dccdbcaa7c06193638ba129ce3b129a8d9c7
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-01-17 17:30:19 -0300

    Adiciona requisito 3

---
+++ src/pages/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import * as userRequests from '../services/userAPI';
+import Loading from './Loading';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      registeredUser: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getName();
+  }
+
+  getName = () => {
+    this.setState({ loading: true }, async () => {
+      const registeredUserObject = await userRequests.getUser();
+      const result = registeredUserObject.name;
+      this.setState({ loading: false, registeredUser: result });
+    });
+  }
+
+  render() {
+    const {
+      loading,
+      registeredUser,
+    } = this.state;
+
+    return (
+      loading ? <Loading />
+        : (
+          <header data-testid="header-component">
+            <p data-testid="header-user-name">{`Olá ${registeredUser}`}</p>
+          </header>)
+    );
+  }
+}
+
+export default Header;
