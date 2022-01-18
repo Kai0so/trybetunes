21c8a6106e134c60b1161356fb7f86c5d98c7b85
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-01-18 14:00:22 -0300

    Adiciona requisito 6 e ajustes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
@@ -7,7 +10,19 @@ class Search extends React.Component {
     this.state = {
       searchInput: '',
       searchButtonDisabled: true,
+      loading: true,
+      albums: [],
+      artistToSearch: '',
+      resultMessage: '',
     };
+  }
+
+  componentDidMount() {
+    this.stopLoading();
+  }
+
+  stopLoading = () => {
+    this.setState({ loading: false });
   }
 
   handleChange = ({ target: { value } }) => {
@@ -28,30 +43,91 @@ class Search extends React.Component {
     }
   }
 
+  handleClick = () => {
+    const {
+      searchInput,
+    } = this.state;
+
+    this.setState({ loading: true, artistToSearch: searchInput }, this.findAlbuns);
+  }
+
+  findAlbuns = async () => {
+    const {
+      searchInput,
+      artistToSearch,
+    } = this.state;
+
+    const MIN_RESULT_LENGTH = 1;
+
+    const result = await searchAlbumsAPI(searchInput);
+    if (result.length >= MIN_RESULT_LENGTH) {
+      this.setState({
+        albums: result,
+        searchInput: '',
+        resultMessage: `Resultado de álbuns de: ${artistToSearch}`,
+        loading: false,
+      });
+    } else {
+      this.setState({
+        albums: [],
+        searchInput: '',
+        resultMessage: 'Nenhum álbum foi encontrado',
+        loading: false,
+      });
+    }
+  }
+
   render() {
     const {
       searchInput,
       searchButtonDisabled,
+      loading,
+      albums,
+      resultMessage,
     } = this.state;
 
     return (
-      <div data-testid="page-search">
-        <Header />
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ searchInput }
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="search-artist-button"
-          disabled={ searchButtonDisabled }
-        >
-          Pesquisar
-        </button>
-      </div>
-    );
+      loading ? <Loading /> : (
+        <div data-testid="page-search">
+          <Header />
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              value={ searchInput }
+              onChange={ this.handleChange }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ searchButtonDisabled }
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+          </form>
+          <span>
+            { resultMessage }
+          </span>
+          <br />
+          { albums.map((album) => (
+            <li key={ album.collectionId }>
+              <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+              <br />
+              <Link
+                to={ `/album/${album.collectionId}` }
+                data-testid={ `link-to-album-${album.collectionId}` }
+              >
+                Músicas
+              </Link>
+              <br />
+              <span>{ album.collectionName }</span>
+              <br />
+              <span>{ album.artistName }</span>
+            </li>
+          ))}
+        </div>
+      ));
   }
 }
 
