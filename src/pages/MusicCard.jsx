c12cdef3ec2d7c4082b7852c425e20bb15c61728
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-01-24 00:20:08 -0300

    Lint requisito 8

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -8,55 +8,50 @@ class MusicCard extends React.Component {
     super();
     this.state = {
       loading: false,
-      songState: '',
       favorite: false,
     };
   }
 
   handleclick = async () => {
-    const {song} = this.props;
+    const { song } = this.props;
     this.setState({ loading: true });
-    await addSong(song)
-    this.setState({loading: false, favorite: true});
-
+    await addSong(song);
+    this.setState({ loading: false, favorite: true });
   }
-
 
   render() {
     const {
       trackName,
       preview,
       trackId,
-      song,
     } = this.props;
 
     const { loading, favorite } = this.state;
 
     return (
-      <>
-      {loading ? <Loading /> 
-        :
       <div>
-        <h4>
-          { trackName }
-        </h4>
-        <audio data-testid="audio-component" src={ preview } controls>
-          <track kind="captions" />
-        </audio>
-        <label htmlFor="favorite">
-          Favorita
-          <input
-            type="checkbox"
-            name="favorite"
-            id="favorite"
-            onClick={this.handleclick}
-            defaultChecked={ favorite }
-            data-testid={`checkbox-music-${trackId}`}
-          />
-        </label>
+        {loading ? <Loading />
+          : (
+            <div>
+              <h4>
+                { trackName }
+              </h4>
+              <audio data-testid="audio-component" src={ preview } controls>
+                <track kind="captions" />
+              </audio>
+              <label htmlFor="favorite">
+                Favorita
+                <input
+                  type="checkbox"
+                  name="favorite"
+                  id="favorite"
+                  onClick={ this.handleclick }
+                  defaultChecked={ favorite }
+                  data-testid={ `checkbox-music-${trackId}` }
+                />
+              </label>
+            </div>)}
       </div>
-    }
-    </>
     );
   }
 }
